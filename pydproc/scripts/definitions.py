82eb36ac84319bf48b89f3ad9e68fed937523d4a
vlad <vladov3000@gmail.com>
2020-04-10 22:07:30 -0700

    definitions package_dir uses package directory now

--- pydproc/scripts/definitions.py
+++ pydproc/scripts/definitions.py
@@ -4,7 +4,12 @@ from pathlib import Path
 import os
 
 # TODO use glob to find package_dir
-package_dir = Path(os.path.abspath('.'))
+package_dir = Path(os.path.abspath(__file__.rsplit('/', 1)[0] + '/..'))
 docker_base_path = package_dir / "docker_base"
 saved_images_path = package_dir / "saved_images"
 saved_data_path = package_dir / "saved_data"
+
+# Initialize directories
+for p in [saved_images_path, saved_data_path]:
+    if not p.exists():
+        os.mkdir(p)
