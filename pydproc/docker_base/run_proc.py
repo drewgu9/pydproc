4900c3ca4e020f3a6f6e9e18c7dda81e8eed4adc
Robert Burris <35885789+roblburris@users.noreply.github.com>
2020-04-11 00:56:28 -0700

    fixed indentation

--- pydproc/docker_base/run_proc.py
+++ pydproc/docker_base/run_proc.py
@@ -41,25 +41,25 @@ def filter_data(data, fields):
     """
     
     def scrap_fields(data, fields):
-    """
-    Removes data not necessary to the client
-    
-    :param data: dict of the working API data
-    :param fields: are the data fields the client wants saved in a dict
-    """
-    for element in data:
-        working_keys = list(element.keys())
-        cur_keys = list(fields.keys())
-        for key in working_keys:
-            if not isinstance(data[key], dict):
-                if key not in cur_keys:
-                    del data[key]
-            else:
-                if key in cur_keys:
-                    data = __recur_fields(data[key], fields[key])
+        """
+        Removes data not necessary to the client
+
+        :param data: dict of the working API data
+        :param fields: are the data fields the client wants saved in a dict
+        """
+        for element in data:
+            working_keys = list(element.keys())
+            cur_keys = list(fields.keys())
+            for key in working_keys:
+                if not isinstance(data[key], dict):
+                    if key not in cur_keys:
+                        del data[key]
                 else:
-                    data = __recur_fields(data[key], fields)
-        return data
+                    if key in cur_keys:
+                        data = __recur_fields(data[key], fields[key])
+                    else:
+                        data = __recur_fields(data[key], fields)
+            return data
     
     def cleanup(data):
         """
