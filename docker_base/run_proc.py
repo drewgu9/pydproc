8df406fb0982a3327cd10ffce93e15d33131defc
Robert Burris <35885789+roblburris@users.noreply.github.com>
2020-04-10 22:15:44 -0700

    moved helper methods from utils

--- docker_base/run_proc.py
+++ docker_base/run_proc.py
@@ -42,8 +42,49 @@ def filter_data(data, fields):
     :param fields: dict representing the keys/indexes to be saved
     :return: a dict with the parsed data inside
     """
-    clean_data = __cleanup(__scrap_fields(data, fields))
-
+    
+    def scrap_fields(data, fields):
+    """
+    Removes data not necessary to the client
+    
+    :param data: dict of the working API data
+    :param fields: are the data fields the client wants saved in a dict
+    """
+    for element in data:
+        working_keys = list(element.keys())
+        cur_keys = list(fields.keys())
+        for key in working_keys:
+            if not isinstance(data[key], dict):
+                if key not in cur_keys:
+                    del data[key]
+            else:
+                if key in cur_keys:
+                    data = __recur_fields(data[key], fields[key])
+                else:
+                    data = __recur_fields(data[key], fields)
+        return data
+    
+    def cleanup(data):
+        """
+        Remove null values from parsed API data
+        :param data: parsed API data in a dict
+        """
+        if isinstance(data, dict):
+            return_data = {}
+            for key in data.keys():
+                if data[key] is not None:
+                    return_data[key] = __cleanup(data[key])
+            return return_data
+        elif isinstance(data, list):
+            l = []
+            for item in data:
+                l.append(__cleanup(item))
+            return l
+        else:
+            return data
+    
+    clean_data = cleanup(scrap_fields(data, fields))
+    
     fin_data = {}
     for key in list(clean_data.keys()):
         if clean_data[key]:
